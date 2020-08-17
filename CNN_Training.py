3081ac6b0d8c7cbebd56083a9abac0d4d796b311
suriyakrishnan <suriya@outlook.in>
2020-08-17 22:30:10 +0530

    error
    resolving resize error

--- CNN_Training.py
+++ CNN_Training.py
@@ -12,11 +12,24 @@ list = os.listdir(path)
 print(len(list))
 no_of_class=len(list)
 
+#########################
+# importing the folders as class
 for x in range(0,no_of_class-1):
     picture_list=os.listdir(path+"/Number-"+str(x))
     for y in picture_list:
         current_image=cv2.imread(path+"/Number-"+str(x)+"/"+y)
-        #current_image=cv2.resize(current_image,(32,32))
+        print(path+"/Number-"+str(x)+"/"+y)
+        current_image=cv2.resize(current_image,(32,32))
         images.append(current_image)
         class_number.append(x)
-    print(x)
+    print(x,end=" ")
+print(" ")
+#print(len(images)) = print(len(images))
+#########################
+#convert into numpy array
+
+images = np.array(images)
+class_number = np.array(class_number)
+
+print(images.shape)
+print(class_number.shape)
