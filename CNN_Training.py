9a26cf1da04c693ccf3dbd331dca72bb12ffbe5f
suriyakrishnan <suriya@outlook.in>
2020-08-17 14:02:18 +0530

    Initial commit

--- CNN_Training.py
+++ CNN_Training.py
@@ -4,8 +4,19 @@ import os
 
 ################################
 path='data'
-
+images=[]
+class_number=[]
 ###############
 
-myList = os.listdir(path)
-print(len(myList))
+list = os.listdir(path)
+print(len(list))
+no_of_class=len(list)
+
+for x in range(0,no_of_class-1):
+    picture_list=os.listdir(path+"/Number-"+str(x))
+    for y in picture_list:
+        current_image=cv2.imread(path+"/Number-"+str(x)+"/"+y)
+        #current_image=cv2.resize(current_image,(32,32))
+        images.append(current_image)
+        class_number.append(x)
+    print(x)
