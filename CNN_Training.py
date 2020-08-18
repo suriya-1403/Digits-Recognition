72a9bc0931f6dab18c0b180e7d041c00a9d36515
suriyakrishnan <suriya@outlook.in>
2020-08-18 12:42:22 +0530

    Update CNN_Training.py
    Started to split the data

--- CNN_Training.py
+++ CNN_Training.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
 import os
+from sklearn.model_selection import train_test_split
 
 ################################
 path='data'
 images=[]
+test_ratio=0.2
 class_number=[]
 ###############
 
@@ -18,7 +20,7 @@ for x in range(0,no_of_class):
     picture_list=os.listdir(path+"/Number-"+str(x))
     for y in picture_list:
         current_image=cv2.imread(path+"/Number-"+str(x)+"/"+y)
-        print(path+"/Number-"+str(x)+"/"+y)
+        #print(path+"/Number-"+str(x)+"/"+y)
         current_image=cv2.resize(current_image,(32,32))
         images.append(current_image)
         class_number.append(x)
@@ -32,4 +34,12 @@ images = np.array(images)
 class_number = np.array(class_number)
 
 print(images.shape)
-print(class_number.shape)
+#print(class_number.shape)
+
+#########################################
+#spliting the data
+#20%testing and 80%training
+x_train,x_test,y_train,y_test = train_test_split(images,class_number,test_size=0.2)
+
+print(x_train.shape)
+print(x_test.shape)
