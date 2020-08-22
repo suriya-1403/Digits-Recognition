c4ff00c18f2f64fdb1d224b9f84c3423161b5469
suriyakrishnan <suriya@outlook.in>
2020-08-22 13:32:31 +0530

    Update CNN_Training.py
    Added CNN

--- CNN_Training.py
+++ CNN_Training.py
@@ -1,8 +1,14 @@
 import cv2
 import numpy as np
 import os
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Flatten
+from keras.optimizers import Adam
+from keras.layers.convolutional import Conv2D,MaxPooling2D
 
 ################################
 path='data'
@@ -11,7 +17,7 @@ test_ratio=0.2
 val_ratio=0.2
 class_number=[]
 no_of_samples=[]
-
+image_Dimension=[32,32]
 ###############
 
 _list_ = os.listdir(path)
@@ -75,8 +81,55 @@ def preProcessing(img):
 
 # x_train=np.array(lists(map(preProcessing,x_train)))
 x_train= np.array(list(map(preProcessing,x_train)))
-img = x_train[30]
-img=cv2.resize(img,(300,300))
-cv2.imshow("PreProcessed",img)
-cv2.waitKey(0)
-print(x_train[30].shape)
+# img = x_train[30]
+# img=cv2.resize(img,(300,300))
+# cv2.imshow("PreProcessed",img)
+# cv2.waitKey(0)
+# print(x_train[30].shape)
+x_test=np.array(list(map(preProcessing,x_test)))
+x_validation=np.array(list(map(preProcessing,x_validation)))
+
+#print(x_train.shape)
+x_train=x_train.reshape(x_train.shape[0],x_train.shape[1],x_train.shape[2],1)
+#print(x_train.shape)
+x_test=x_test.reshape(x_test.shape[0],x_test.shape[1],x_test.shape[2],1)
+x_validation=x_validation.reshape(x_validation.shape[0],x_validation.shape[1],x_validation.shape[2],1)
+
+#
+data_Generator = ImageDataGenerator(width_shift_range=0.1,
+                                    height_shift_range=0.1,
+                                    zoom_range=0.2,
+                                    shear_range=0.1,
+                                    rotation_range=10)
+
+data_Generator.fit(x_train)
+
+y_train=to_categorical(y_train,no_of_class)
+y_test=to_categorical(y_test,no_of_class)
+y_validation=to_categorical(y_validation,no_of_class)
+################################################################################################
+def _model_():
+    no_of_filters = 60
+    size_of_filter=(5,5)
+    size_of_filter2=(3,3)
+    size_of_pool=(2,2)
+    no_of_node=500
+
+    model=Sequential()
+    model.add((Conv2D(no_of_filters,size_of_filter,input_shape=(image_Dimension[0],image_Dimension[1],1),activation='relu')))
+    model.add((Conv2D(no_of_filters,size_of_filter,activation='relu')))
+    model.add(MaxPooling2D(pool_size=size_of_pool))
+    model.add((Conv2D(no_of_filters//2,size_of_filter2,activation='relu')))
+    model.add((Conv2D(no_of_filters//2,size_of_filter2,activation='relu')))
+    model.add(MaxPooling2D(pool_size=size_of_pool))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    model.add(Dense(no_of_node,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(no_of_class,activation='softmax'))
+    model.compile(Adam(lr=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
+    return model
+
+model=_model_()
+print(model.summary())
